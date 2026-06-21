16da97d4d533042cee9823409d0d7ecac5888466
jesjoha <jesper.johans@gmail.com>
2026-06-21 22:02:26 +0300

    compose.yaml starts database, database migrations and backend server now.

---
+++ foodplanner-server/src/main/java/me/jesjoha/foodplannerserver/recipe/CookingInstructions.java
@@ -0,0 +1,11 @@
+package me.jesjoha.foodplannerserver.recipe;
+
+import java.util.List;
+
+public class CookingInstructions {
+    private List<String> steps;
+
+    public CookingInstructions(List<String> steps) {
+        this.steps = steps;
+    }
+}
--- foodplanner-server/src/main/java/me/jesjoha/foodplannerserver/recipe/Recipe.java
+++ foodplanner-server/src/main/java/me/jesjoha/foodplannerserver/recipe/Recipe.java
@@ -1,4 +1,12 @@
 package me.jesjoha.foodplannerserver.recipe;
 
 public class Recipe {
+    private int id;
+    private String name;
+    private String description;
+
+    public Recipe(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
 }
