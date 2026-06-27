8202e8f1815a516a2a98698a0242feaf6021bfae
jesjoha <jesper.johans@gmail.com>
2026-06-27 22:08:38 +0300

    Added client which required some dependency trickery...

---
+++ client/src/components/Users.svelte
@@ -0,0 +1,26 @@
+<script>
+    const API_URL = "http://localhost:8000/api/v1";
+
+    let users = $state([]);
+
+    const getUsers = async () => {
+        const response = await fetch(`${API_URL}/users`);
+        users = await response.json();
+    }
+
+
+    $effect(() => {
+        getUsers();
+    })
+
+</script>
+
+<h3>Users:</h3>
+
+{#if users}
+    <ul>
+        {#each users as user}
+            <li>{user.email}</li>
+        {/each}
+    </ul>
+{/if}
